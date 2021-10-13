626a64c9852e606faaf5e3ca8df5a74a410d4ae5
juankmilo06 <38663735+juankmilo06@users.noreply.github.com>
2021-10-12 20:00:35 -0500

    Add files via upload

---
+++ cuevas/html.php
@@ -0,0 +1,14 @@
+<?php
+/*$addNombreC=$_POST["Nombre del Cliente"];
+$addApellidoC=$_POST["Nombre del Cliente"];
+$addDireccionC=$_POST["Nombre del Cliente"];
+$addIdentificacionC=$_POST["Nombre del Cliente"];
+$addCiuadadC=$_POST["Nombre del Cliente"];
+$addTelefonoC=$_POST["Nombre del Cliente"];*/
+
+echo "<br>".$_POST["NombreC"];
+echo "<br>".$_POST["ApellidoC"];
+echo "<br>".$_POST["IdentificacionC"];
+echo "<br>".$_POST["CiudadC"];
+echo "<br>".$_POST["TelefonoC"];
+?>
---
+++ cuevas/php.php
@@ -0,0 +1,19 @@
+<?php
+$addNombreC=$_POST["NombreC"];
+$addApellidoC=$_POST["ApelidoC];
+$addDireccionC=$_POST["DireccionC"];
+$addIdentificacionC=$_POST["IdentificacionC"];
+$addCiuadadC=$_POST["CiudadC"];
+$addTelefonoC=$_POST["TelefonoC"];
+
+echo "<br>".$_POST["NombreC"];
+echo "<br>".$_POST["ApellidoC"];
+echo "<br>".$_POST["IdentificacionC"];
+echo "<br>".$_POST["CiudadC"];
+echo "<br>".$_POST["TelefonoC"];
+
+
+
+
+
+?>
