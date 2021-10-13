adca1ff4960d46f40f7ca9174c0868c0f8e68c81
juankmilo06 <38663735+juankmilo06@users.noreply.github.com>
2021-10-12 20:22:01 -0500

    Add files via upload

--- modulos/pagos/consulta.php
+++ modulos/pagos/consulta.php
@@ -1,35 +1,40 @@
 <?php
 include "../../seguridad/seguridad.php";
 include "../../conexion/conexion.php";
+
 try{
     //$conexion = new PDO('mysql:host=localhost;dbname=u858238889_park','u858238889_root','parkingdom',array( PDO::ATTR_EMULATE_PREPARES=>false,
-    //PDO::MYSQL_ATTR_DIRECT_QUERY=>false,
-    //PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION
+//    PDO::MYSQL_ATTR_DIRECT_QUERY=>false,
+  //  PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION
 //));
 
     date_default_timezone_set("America/Bogota");
-//$tipovehiculo="Automovil";
-
-    $placa=$_POST['placa'];
-    //$placa="epr987";
-
-        $statement = $conexion->prepare("SELECT *
-FROM vehiculos WHERE placa_vehi =  '$placa'");
+	$placa=$_POST['placa'];
+    //$placa="epr8d";
+		$fecha_actual=date("Y-m-d H:i:s");  
+		$usuario=$_SESSION['usuario'];
+		$espacio= 10;
+        $statement = $conexion->prepare("SELECT a . * , b.ubicacion
+		FROM vehiculos AS a
+		LEFT JOIN vehiculo_ubicacion AS b ON a.placa_vehi = b.placa
+		WHERE placa_vehi =  '$placa'");
         $statement->execute();
         $resultados = $statement->fetch();
 	
+        $envio = array();
         if($resultados==TRUE){
-			if($resultados['tipo_vehiculo'] == "automovil"){
-				$tarifa = '70000';
-			}else if($resultados['tipo_vehiculo'] == "motocicleta"){
-				$tarifa = '40000';
-			}
-			echo $tarifa;
+           $tipo_cliente = 'mensual'; 
+			$envio["cliente"] = $tipo_cliente;
+			$envio["lugar"] = $resultados['ubicacion'];
+			$envio["tipo_vehiculo"] = $resultados['tipo_vehiculo'];
+			
            //echo 'mensual';
         }else{
-         $error= 'error'; 
-           echo 'error';
+         $tipo_cliente= 'temporal'; 
+         $envio["cliente"] = $tipo_cliente;
+
         }
+	echo json_encode($envio);
     //	echo $tipovehiculo;
 	}catch(PDOException $e){
     echo "Error: " . $e->getMessage();
